9409fa96f5eb311c1a16bfef43c8b5f883005446
Mengwei Choong <choongmengwei@gmail.com>
2018-01-23 16:07:03 +0100

    core added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component }       from '@angular/core';
 
-import { QuestionService } from './question.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css',],
-  providers:  [QuestionService]
 })
 export class AppComponent {
-  questions: any[];
-
-  constructor(service: QuestionService) {
-    this.questions = service.getQuestions();
+  constructor() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule }          from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { MessageService }  from './message.service'
-import { QuestionService }  from './question.service'
-import { DynamicFormComponent }         from './dynamic-form/dynamic-form.component';
-import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  imports: [ BrowserModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, DynamicFormComponent, DynamicFormQuestionComponent ],
+  imports: [
+    CoreModule,
+  ],
+  declarations: [ AppComponent ],
   bootstrap: [ AppComponent ],
-  providers: [ QuestionService ],
 })
 export class AppModule {
-  constructor() {
-  }
+  constructor() {}
 }
